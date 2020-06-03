1c912f28f577b09627ae682a309f46a6a58c7259
Karan Modh <karanmodh71@gmail.com>
2020-06-03 14:11:54 +0530

    Changed Directory Structure

--- sms_proj/settings.py
+++ sms_proj/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -29,7 +30,7 @@ SECRET_KEY = '=fcmlu7+ycm4)1+b$zwhcqdlaxf!)k*29b29hy622+3hfsqn0i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sms-app-cs321.herokuapp.com']
+ALLOWED_HOSTS = ['sms-app-cs321.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -78,7 +79,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'sms_proj.wsgi.application'
 
 # Form Template
-CRISPY_TEMPLATE_PACK="bootstrap4"
+CRISPY_TEMPLATE_PACK= "bootstrap4"
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
@@ -127,10 +128,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/staticfiles/'
 STATICFILES_DIR = [STATIC_DIR, ]
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
@@ -138,3 +139,5 @@ MEDIA_URL = '/media/'
 LOGIN_URL = '/login/'
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+django_heroku.settings(locals())
